9c265ac5234122879680926b277ec1955ce127c1
Rizzzky <114666981+Rizzzky@users.noreply.github.com>
2024-10-24 22:53:21 +0700

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="number" v-model="bilangan1" placeholder="Isikan Bilangan 1">
+    +
+    <input type="number" v-model="bilangan2" placeholder="Isikan Bilangan 2">
+    =
+    <input type="number" :value="hasil" disabled>
+    <button @click="hitung">HITUNG</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bilangan1: 0,
+      bilangan2: 0,
+      hasil: 0
+    }
+  },
+  methods: {
+    hitung() {
+      this.hasil = this.bilangan1 + this.bilangan2;
+    }
+  }
+}
+</script>
